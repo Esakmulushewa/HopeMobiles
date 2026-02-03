140d53775bc3d4d5827653b3a5fd072ea9dc2fea
Esakmulushewa <123089710+Esakmulushewa@users.noreply.github.com>
2026-02-04 02:50:48 +0300

    Change database configuration to use Django settings

--- HopeMobiles/settings.py
+++ HopeMobiles/settings.py
@@ -74,8 +74,10 @@ WSGI_APPLICATION = 'HopeMobiles.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-     'default': dj_database_url.config(
-        default='sqlite:///db.sqlite3'
+     'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
     )
 }
 
