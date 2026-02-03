fd59e7ac0eaaeaaeb6c9f6cb21ee0444f5ee40b1
Esakmulushewa <123089710+Esakmulushewa@users.noreply.github.com>
2026-02-04 01:48:31 +0300

    Enhance build script with pip upgrade and installs
    Added pip upgrade and installation steps before collecting static files and running migrations.

--- build.sh
+++ build.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
-set -o errexit
+set -o errexit  # Exit on first error
 
+# Upgrade pip to avoid wheel/build issues
+pip install --upgrade pip
+
+# Install Python dependencies
 pip install -r requirements.txt
+
+# Collect static files
 python manage.py collectstatic --noinput
+
+# Run migrations
 python manage.py migrate
